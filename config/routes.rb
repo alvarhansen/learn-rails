5ec1ec15015fc57f44d94540b498ed9388a9c49f
Alvar Hansen <alluxxii@gmail.com>
2013-12-11 16:28:19 +0200

    mailing list

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,6 @@ LearnRails::Application.routes.draw do
   #     resources :products
   #   end
   resources :contacts, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
 end
